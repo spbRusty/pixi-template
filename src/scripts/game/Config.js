2c2899664add54d77c1b9b81430d3abb6c24195f
spbRusty <42145047+spbRusty@users.noreply.github.com>
2024-09-01 15:08:12 +0300

    Update Config.js

--- src/scripts/game/Config.js
+++ src/scripts/game/Config.js
@@ -2,9 +2,9 @@ import { Game } from "./Game";
 
 export const Config = {
     loader: [
-        { key: "player", data: "path/to/player.png" },
-        { key: "obstacle", data: "path/to/obstacle.png" },
-        { key: "background", data: "path/to/background.png" }
+        { key: "player", data: "/sprites/player.png" },
+        { key: "obstacle", data: "/sprites/obstacle.png" },
+        { key: "background", data: "/sprites/background.png" }
     ],
     startScene: Game,
     scenes: {
